0dfe4babd5731c39b5d53634af2de37513f79650
bentenlovertherian <jamie.doig.nz@gmail.com>
2024-03-13 19:56:16 +1300

    testing game with basic tile to ensure nothing is wrong with the tilesets

--- main.py
+++ main.py
@@ -32,52 +32,35 @@ class MyGame(arcade.Window):
 
     def __init__(self):
 
-        # Call the parent class and set up the window
+        # Calls the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        # Our TileMap Object
         self.tile_map = None
 
-        # Our Scene Object
         self.scene = None
 
-        # Separate variable that holds the player sprite
         self.player_sprite = None
 
-        # Our physics engine
         self.physics_engine = None
 
-        # A Camera that can be used for scrolling the screen
         self.camera = None
 
-        # A Camera that can be used to draw GUI elements
         self.gui_camera = None
-
-
-        # Where is the right edge of the map?
 
         self.end_of_map = 0
 
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
 
-        # Set up the Cameras
 
         self.camera = arcade.Camera(self.width, self.height)
 
         self.gui_camera = arcade.Camera(self.width, self.height)
 
 
-
-        # Map name
-
         #map_name = f":resources:tiled_maps/map2_level_{self.level}.json"
         map_name = "maps/map1.tmx"
 
-
-
-        # Layer Specific Options for the Tilemap
 
         layer_options = {
 
@@ -93,29 +76,21 @@ class MyGame(arcade.Window):
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
 
-        # Set up the player, specifically placing it at these coordinates.
         image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = PLAYER_START_X
-
         self.player_sprite.center_y = PLAYER_START_Y
-
         self.scene.add_sprite("Player", self.player_sprite)
 
 
-        # --- Load in a map from the tiled editor ---
-
-        # Calculate the right edge of the my_map in pixels
         self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
 
-        # --- Other stuff
-        # Set the background color
+    
         if self.tile_map.background_color:
 
             arcade.set_background_color(self.tile_map.background_color)
 
 
-        # Create the 'physics engine'
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player_sprite,
             gravity_constant=GRAVITY,
@@ -123,34 +98,26 @@ class MyGame(arcade.Window):
         )
 
     def on_draw(self):
-        """Render the screen."""
 
-        # Clear the screen to the background color
         self.clear()
 
-        # Activate the game camera
         self.camera.use()
 
-        # Draw our Scene
         self.scene.draw()
 
-        # Activate the GUI camera before drawing GUI elements
         self.gui_camera.use()
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
 
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
-                arcade.play_sound(self.jump_sound)
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key."""
 
         if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
@@ -171,12 +138,9 @@ class MyGame(arcade.Window):
         self.camera.move_to(player_centered)
 
     def update(self, delta_time):
-        """Movement and game logic"""
 
-        # Move the player with the physics engine
         self.physics_engine.update()
 
-        # Did the player fall off the map?
 
         if self.player_sprite.center_y < -100:
 
